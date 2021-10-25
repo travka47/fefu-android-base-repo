1fea96a11fcebf62c0311f267f5d5dda44aee379
travka47 <travka.eal@students.dvfu.ru>
2021-10-26 01:43:43 +1000

    Created Tab Layout using ViewPager2

--- app/src/main/kotlin/ru/fefu/fitnes_tracker/main/fragments/ActivityFragment.kt
+++ app/src/main/kotlin/ru/fefu/fitnes_tracker/main/fragments/ActivityFragment.kt
@@ -5,7 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.fragment_activity.*
 import ru.fefu.activitytracker.R
+import ru.fefu.fitnes_tracker.main.ViewPagerAdapter
 
 class ActivityFragment : Fragment() {
 
@@ -18,6 +25,22 @@ class ActivityFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_activity, container, false)
+        val myFragment = inflater.inflate(R.layout.fragment_activity, container, false)
+        val tabLayout = myFragment.findViewById<TabLayout>(R.id.fr_act_tabLayout)
+        val viewPager2 = myFragment.findViewById<ViewPager2>(R.id.fr_act_viewPager)
+        val adapter = ViewPagerAdapter(childFragmentManager, lifecycle)
+        viewPager2.adapter = adapter
+        TabLayoutMediator(tabLayout, viewPager2) {tab, position->
+            when(position) {
+                0->{
+                    tab.text = "Моя"
+                }
+                1->{
+                    tab.text = "Пользователей"
+                }
+            }
+        }.attach()
+
+        return myFragment
     }
 }
