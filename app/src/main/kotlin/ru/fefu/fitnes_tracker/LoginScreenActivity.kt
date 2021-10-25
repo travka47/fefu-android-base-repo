71caa7236a5f8dfb16795fce1032881b54a8df07
travka47 <travka.eal@students.dvfu.ru>
2021-10-25 23:34:17 +1000

    Fixed naming

--- app/src/main/kotlin/ru/fefu/fitnes_tracker/LoginScreenActivity.kt
+++ app/src/main/kotlin/ru/fefu/fitnes_tracker/LoginScreenActivity.kt
@@ -3,17 +3,17 @@ package ru.fefu.fitnes_tracker
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import ru.fefu.activitytracker.databinding.LoginBinding
+import ru.fefu.activitytracker.databinding.LoginScreenBinding
 
-class loginActivity: AppCompatActivity() {
-    lateinit var binding: LoginBinding
+class LoginScreenActivity: AppCompatActivity() {
+    lateinit var binding: LoginScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = LoginBinding.inflate(layoutInflater)
+        binding = LoginScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.logImageButtonArrow.setOnClickListener {
-            val intent = Intent(this, welcomeScreenActivity::class.java)
+            val intent = Intent(this, WelcomeScreenActivity::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/kotlin/ru/fefu/fitnes_tracker/WelcomeScreenActivity.kt
+++ app/src/main/kotlin/ru/fefu/fitnes_tracker/WelcomeScreenActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import ru.fefu.activitytracker.databinding.WelcomeScreenBinding
 
-class welcomeScreenActivity: AppCompatActivity() {
+class WelcomeScreenActivity: AppCompatActivity() {
     lateinit var binding: WelcomeScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,12 +13,12 @@ class welcomeScreenActivity: AppCompatActivity() {
         setContentView(binding.root)
 
         binding.wsButtonRegister.setOnClickListener {
-            val intent = Intent(this, registrationActivity::class.java)
+            val intent = Intent(this, RegistrationScreenActivity::class.java)
             startActivity(intent)
         }
 
         binding.wsButtonLogin.setOnClickListener {
-            val intent = Intent(this, loginActivity::class.java)
+            val intent = Intent(this, LoginScreenActivity::class.java)
             startActivity(intent)
         }
     }
