926d64038c0c2595e1048e2ae901a47af638b893
travka47 <travka.eal@students.dvfu.ru>
2021-10-26 00:15:11 +1000

    Started bottom navigation

---
+++ app/src/main/kotlin/ru/fefu/fitnes_tracker/main/MainScreenActivity.kt
@@ -0,0 +1,12 @@
+package ru.fefu.fitnes_tracker.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.fefu.activitytracker.R
+
+class MainScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_screen)
+    }
+}
