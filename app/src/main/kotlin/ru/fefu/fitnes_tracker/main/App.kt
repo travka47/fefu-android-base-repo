c611ee47a7016f702d5fa90de4da235e89ed800d
travka47 <travka.eal@students.dvfu.ru>
2021-12-07 21:48:50 +1000

    Mega-commit

---
+++ app/src/main/kotlin/ru/fefu/fitnes_tracker/main/App.kt
@@ -0,0 +1,27 @@
+package ru.fefu.fitnes_tracker.main
+
+import android.app.Application
+import androidx.room.Room
+import ru.fefu.fitnes_tracker.main.room.db.MyActivitiesDatabase
+import ru.fefu.fitnes_tracker.main.room.math.Converters
+
+class App : Application() {
+
+    companion object {
+        lateinit var INSTANCE: App
+    }
+
+    val db by lazy {
+        Room.databaseBuilder(
+            this,
+            MyActivitiesDatabase::class.java,
+            "my_activities_database"
+        ).allowMainThreadQueries().build()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        INSTANCE = this
+    }
+}
